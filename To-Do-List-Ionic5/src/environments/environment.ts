8c7f45a3052da6697443d6f5919b5fc703f8ee5e
Charbel Daher <charbel07@yahoo.com.br>
2022-09-26 15:31:06 -0300

    Configs iniciais

--- To-Do-List-Ionic5/src/environments/environment.ts
+++ To-Do-List-Ionic5/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCoq_JN1fGRrM4fuBPDL0Z15IpXg9DnExA",
+    authDomain: "fir-ionic-b3e45.firebaseapp.com",
+    projectId: "fir-ionic-b3e45",
+    storageBucket: "fir-ionic-b3e45.appspot.com",
+    messagingSenderId: "535028707928",
+    appId: "1:535028707928:web:c0ce5e65e8a0633a66102d",
+    measurementId: "G-EJNLVGMY9Z"
+  }
 };
 
 /*
